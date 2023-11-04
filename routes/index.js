bc7c215ea653a88ff8c75d4c661e3c976749932c
lizardN <blutech02@gmail.com>
2023-11-04 15:50:58 +0200

    jjj

--- routes/index.js
+++ routes/index.js
@@ -3689,7 +3689,7 @@ const page = await browser.newPage()
 //create a pdf document
 
 await page.pdf({
-  path:('./reports/'+year+'/'+month),
+  path:('./public/reports/'+year+'/'+month+'/'+uid+'.pdf'),
   format:"A4",
   printBackground:true
 })
