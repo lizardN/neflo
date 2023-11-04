9b7e3744de901ea2b7f5e6755afd07b67de09d0a
lizardN <blutech02@gmail.com>
2023-11-04 08:19:21 +0200

    chromium

--- routes/index.js
+++ routes/index.js
@@ -3662,7 +3662,19 @@ const compile = async function (templateName, arr){
  (async function(){
 
 try{
-const browser = await puppeteer.launch();
+//const browser = await puppeteer.launch();
+const browser = await puppeteer.launch({
+  args: [
+    "--disable-setuid-sandbox",
+    "--no-sandbox",
+    "--single-process",
+    "--no-zygote",
+  ],
+  executablePath:
+    process.env.NODE_ENV === "production"
+      ? process.env.PUPPETEER_EXECUTABLE_PATH
+      : puppeteer.executablePath(),
+});
 
 const page = await browser.newPage()
 
